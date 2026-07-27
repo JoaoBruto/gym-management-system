ca66ccfb6863647862154bfea9127c8076241ec9
Joao Rodrigo <joaofreire185710@gmail.com>
2026-07-27 19:45:20 -0300

    Correção de erros de sintaxe

--- main.py
+++ main.py
@@ -7,5 +7,21 @@ conexao = pymysql.connect(
     database = 'gym_management_db'
 )
 
-cursor = conexao.cursor()
-cursor.execute("ALTER TABLE alunos MODIFY COLUMN mensalidade_paga BOOLEAN NOT NULL DEFAULT FALSE ")
+def cadastrar_aluno():
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: ")) #Converter numero para inteiro
+    telefone = input("Digite seu telefone: ")
+    peso = float(input("Digite seu peso (kg): "))
+    altura = float(input("Digite sua altura (m): "))
+    objetivo = input("Digite seu objetivo: ")
+
+    
+    cursor = conexao.cursor()
+    com_sql = "INSERT INTO alunos(nome, idade, telefone, peso, altura, objetivo, mensalidade_paga) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    valor = nome, idade, telefone, peso, altura, objetivo, True
+    cursor.execute(com_sql, valor)
+
+    conexao.commit()
+
+    print(cursor.rowcount, "Inseridas com sucesso")
+cadastrar_aluno()
