2452a91a73cc0cc7a2c1a420506511dbc84870ce
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-05 14:33:30 -0300

    Criação da atualização de peso

--- main.py
+++ main.py
@@ -182,5 +182,16 @@ def atualizar_aluno():
             conexao.commit()
             print("Telefone atualizado com sucesso!")
 
+
+        elif escolha_atualizacao == "4":
+            while True:
+                try:
+                    novo_peso = float(input("Digite o novo peso do usuário: \n"))
+                    if novo_peso < 10 or novo_peso > 450:
+                        print("Digite um peso válido!")
+                    else:
+                        break
+                except ValueError:
+                    print("Entrada invalida! Por favor, digite apenas números.")
 atualizar_aluno()
         
