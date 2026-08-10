934a8524c00bba559e1be1f16bb373867385b810
Joao Rodrigo <joaofreire185710@gmail.com>
2026-08-10 14:39:15 -0300

    Criação da função de MENU no terminal

--- main.py
+++ main.py
@@ -283,4 +283,11 @@ def excluir_aluno():
             print("Exclusão cancelada!")
         else:
             print("Digite apenas as opções válidas (s ou n) \n")
-#excluir_aluno()            
+#excluir_aluno()    
+
+def menu():
+    opcoes_validas = ["1", "2", "3", "4", "5", "6", "7"]
+    while True:
+        try:
+            opcoes = input("Selecione entre: 1- Cadastrar \n 2- Listar \n 3- Buscar por ID \n 4- Buscar por mensalidade \n 5- Atualizar \n 6- Excluir \n 7- Sair \n")
+        except ValueError:
